f3867bc85ba93c1406a077b173e6132dfb816655
EshaShinde <72072755+EshaShinde@users.noreply.github.com>
2020-11-28 16:14:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,225 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body; 
+const Constraint = Matter.Constraint;
+
+var engine,world;
+var ground1,ground2,ground3,ground4;
+var holder;
+var box1;
+var ball;
+var polygon;
+var sling;
+var polygonIMG;
+var  score = 0;
+var bg = "sprites/bg1.jpg"
+
+function preload()
+{
+   polygonIMG=loadImage("yellow.png");
+  // getBackgroundImg();
+
+}
+
+function setup() {
+  createCanvas(1200,600);
+
+  engine = Engine.create();
+  world = engine.world;
+  Engine.update(engine);
+
+  //ball = createSprite(100,200,20)
+
+  ground1 = new Ground(500,520,250,20);
+  ground2 = new Ground(900,250,250,20);
+  ground3 = new Ground(600,750,1200,20);
+  ground4 = new Ground(80,590,2500,20);
+
+  box1 = new Box(410,500,30,40);
+  box2 = new Box(440,500,30,40);
+  box3 = new Box(470,500,30,40);
+  box4 = new Box(500,500,30,40);
+  box5 = new Box(530,500,30,40);
+
+  box6 = new Box(560,500,30,40);
+  box7 = new Box(590,500,30,40);
+  box8 = new Box(440,460,30,40);
+  box9 = new Box(470,460,30,40);
+  box10 = new Box(500,460,30,40);
+
+  box11 = new Box(530,460,30,40);
+  box12 = new Box(560,460,30,40);
+  box13 = new Box(470,420,30,40);
+  box14 = new Box(500,420,30,40);
+  box15 = new Box(530,420,30,40);
+
+  box16 = new Box(500,380,30,40);
+  box17 = new Box(840,225,30,40);
+  box18 = new Box(870,225,30,40);
+  box19 = new Box(900,225,30,40);
+  box20 = new Box(930,225,30,40);
+
+  box21 = new Box(960,225,30,40);
+  box22 = new Box(870,185,30,40);
+  box23 = new Box(900,185,30,40);
+  box24 = new Box(930,185,30,40);
+  box25 = new Box(900,145,30,40);
+  ball = Bodies.circle(80,530,20);
+  World.add(world,ball);
+
+  slingshot = new Slingshot(this.ball,{x:100,y:200});
+
+  
+
+    
+  Engine.run(engine);
+}
+
+function draw() {
+ background(0);
+
+  noStroke();
+  textSize(25)
+  fill("white")
+  text("SCORE = "+ score, 740, 40)
+  
+  Engine.update(engine);
+
+  ground1.display();
+  ground2.display();
+  ground3.display();
+  ground4.display();
+
+  fill("lightgreen");
+
+  box1.display();
+  box1.score();
+
+  box2.display();
+  box2.score();
+
+  box3.display();
+  box3.score();
+
+  box4.display();
+  box4.score();
+
+  box5.display();
+  box5.score();
+
+  box6.display();
+  box6.score();
+
+  box7.display();
+  box7.score();
+
+
+  fill("lightblue");
+  box8.display();
+  box8.score();
+
+  box9.display();
+  box9.score();
+
+  box10.display();
+  box10.score();
+
+  box11.display();
+  box11.score();
+
+  box12.display();
+  box12.score();
+
+  fill("pink")
+  box13.display();
+  box13.score();
+
+  box14.display();
+  box14.score();
+
+  box15.display();
+  box15.score();
+
+  fill("orange");
+  box16.display();
+  box16.score();
+
+  fill("lightgreen");
+  box17.display();
+  box17.score();
+
+  box18.display();
+  box18.score();
+
+  box19.display();
+  box19.score();
+
+  box20.display();
+  box20.score();
+
+  box21.display();
+  box21.score();
+
+  fill("lightblue");
+  box22.display();
+  box22.score();
+
+  box23.display();
+  box23.score();
+
+  box24.display();
+  box24.score();
+  
+  fill("pink");
+  box25.display();
+  box25.score();
+
+
+  slingshot.display();
+
+  imageMode(CENTER)
+  image(polygonIMG ,ball.position.x,ball.position.y,40,40);
+
+  
+  
+}
+
+function mouseDragged(){
+  Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
+}
+
+function mouseReleased(){
+  slingshot.fly();
+
+} 
+
+function keyPressed(){
+  if(keyCode === 32)
+  {
+      slingshot.attach(this.ball);
+  }}
+
+  //async function getBackgroundImg(){ 
+    //var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    //var responseJSON = await response.json();
+
+    //var datetime = responseJSON.datetime;
+    //var hour = datetime.slice(11,13);
+    
+    ///if(hour>=0600 && hour<=1800){
+       /// bg = "sprites/bg1.jpg";
+   // }
+    //else{
+     //   bg = "sprites/bg2.jpg";
+   // }
+
+   /// backgroundImg = loadImage(bg);
+   /// console.log(backgroundImg);
+///}
+  
+async function getBackgroundImg(){
+  var response = await fetch ("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  
+  console.log('response')
+}
